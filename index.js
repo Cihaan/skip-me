076b49c5e9b6bba7acb68a6e9501f911594a074b
Cihaan <cihankafadar1@gmail.com>
2023-01-11 18:49:15 +0100

    stuff

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
-function onSkipClick() {
-  console.log("skip clicked");
+function selectAnswers() {
+  const showAnswers = document.querySelectorAll(
+    "flex justify-center mt-8 lg:justify-end"
+  );
+
+  for (let aws of showAnswers) {
+    aws.click();
+  }
+
+  const answers = document.querySelectorAll(
+    "flex items-center justify-between pr-5 py-3 rounded-size-24 pl-8 cursor-pointer mb-1 bg-success-05"
+  );
+
+  for (let aws in answers) {
+    aws.click();
+  }
 }
