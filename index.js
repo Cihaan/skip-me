385022fd7c4e67aa23d09dfe906999148e85754d
Cihaan <cihankafadar1@gmail.com>
2023-01-12 05:13:47 +0100

    feat: automate listening exercices next tomorrow zZzZzZzZ

--- index.js
+++ index.js
@@ -1,17 +1,106 @@
-function selectAnswers() {
-  const showAnswers = document.querySelectorAll(
-    "flex justify-center mt-8 lg:justify-end"
-  );
+const exerciceType = document.getElementsByClassName(
+  "font-bold text-size-20 capitalize lg:w-50 lg:truncate"
+)[0]?.innerText;
 
-  for (let aws of showAnswers) {
-    aws.click();
-  }
+console.log(exerciceType);
 
-  const answers = document.querySelectorAll(
-    "flex items-center justify-between pr-5 py-3 rounded-size-24 pl-8 cursor-pointer mb-1 bg-success-05"
-  );
+let progressBar = document.getElementsByClassName(
+  "h-2 bg-primary-80 rounded transition-all duration-150"
+);
 
-  for (let aws in answers) {
-    aws.click();
+async function showAnswers() {
+  if (exerciceType == "Listening") {
+    const showAnswers = document.querySelectorAll(".leading-none");
+
+    for (let aws of showAnswers) {
+      if (aws.innerText == "Voir la correction") {
+        aws.click();
+      }
+    }
+  } else {
+    const showAnswers = document.getElementsByClassName(
+      "button-outline-default-large hidden min-w-48 mr-6 lg:flex lg:items-center"
+    )[0];
+
+    showAnswers.click();
+
+    // flex items-center justify-between pr-5 py-3 rounded-size-24 pl-4 cursor-pointer mb-1 bg-success-05
   }
 }
+
+async function selectAnswers() {
+  if (exerciceType == "Listening") {
+    const answers = document.getElementsByClassName(
+      "flex items-center justify-between pr-5 py-3 rounded-size-24 pl-8 cursor-pointer mb-1 bg-success-05"
+    );
+
+    for (let aws of answers) {
+      aws.click();
+    }
+  } else {
+    //TODO
+  }
+}
+
+async function validate() {
+  if (exerciceType == "Listening") {
+    const submit = document.getElementsByClassName(
+      "min-w-48 button-solid-primary-large"
+    )[0];
+
+    submit.click();
+  } else {
+    //TODO
+  }
+}
+
+async function later(delay) {
+  return new Promise(function (resolve) {
+    setTimeout(resolve, delay);
+  });
+}
+
+async function submit() {
+  showAnswers().then(() => {
+    selectAnswers().then(() =>
+      validate()
+        .then(() => {
+          goNextExercise();
+        })
+        .catch(() => {
+          goNextExercise();
+        })
+    );
+  });
+}
+
+function goNextExercise() {
+  const nextButton = document.getElementsByClassName(
+    "button-solid-primary-medium lg:mb-6 lg:self-end"
+  )[0];
+
+  nextButton.click();
+}
+
+function skip() {
+  const iteration = document
+    .getElementsByClassName(
+      "text-size-12 text-primary-80 leading-tight rounded lg:bg-primary-50 lg:px-2 lg:py-1 lg:mt-2 lg:absolute lg:progress-bar-arrow-top lg:text-white lg:transition-all lg:duration-150 lg:transform lg:-translate-x-1/2"
+    )[0]
+    ?.innerText.split("/");
+
+  if (iteration) {
+    const count = parseInt(iteration[1]) - parseInt(iteration[0]);
+    console.log({ count, exerciceType });
+
+    for (let i = 0; i < count; i++) {
+      setTimeout(() => {
+        submit();
+      }, i * 1000);
+    }
+  } else {
+    goNextExercise();
+  }
+}
+
+skip();
